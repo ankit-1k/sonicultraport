de924a1be1098606a6c176270074af9c5823243c
ankit-1k <ankitpanda922@gmail.com>
2024-05-11 22:04:03 +0530

    error 303

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Ankit Kumar
+          Ankit Kumar Panda
         </a>
       </header>
     </div>
