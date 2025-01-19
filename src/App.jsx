dbb01d185f7f2ed7bee7dd61debf711e95b8ec97
ankit-1k <ankitpanda922@gmail.com>
2025-01-19 09:52:20 +0530

    test 8

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   const fetchAnnouncement=async ()=>{
     try {
       const response=await axios.get('https://sonicadminbackend.vercel.app/api/getannouncement')
-      setAnnouncement(response)
+      setAnnouncement(response.data)
       console.log('announcements',announcement)
     } catch (error) {
       console.log('error',error)
